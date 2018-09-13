38bb501db7dbc502396566989a0cfb40e6cfb381
Emma M <mdopure@gmail.com>
2018-09-13 13:48:37 +0700

    converted GridList and Page components

--- src/components/GridList.js
+++ src/components/GridList.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'react-emotion';
 
-const GridList = ({ children }) => <div className="grid-list">{children}</div>;
+const Grid = styled('div')({
+  display: 'grid',
+  gridGap: '20px',
+  gridTemplateColumns: 'repeat(auto-fit, 220px)',
+  justifyContent: 'center',
+  margin: '40px'
+});
+
+const GridList = ({ children }) => <Grid>{children}</Grid>;
 
 GridList.propTypes = {
   children: PropTypes.oneOfType([PropTypes.array, PropTypes.object])
--- src/components/Page.js
+++ src/components/Page.js
@@ -1,26 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import styled from 'react-emotion';
 
-export const Page = ({ background, theme, container, children }) => {
-  const bg =
-    background && background.image
-      ? `url('${background.image.urls.full}')`
-      : `linear-gradient(to bottom, ${theme.primary}, ${theme.primary})`;
+const PageContainer = styled('div')({
+  height: '100vh',
+  position: 'relative',
+  width: '100vw'
+});
 
-  return (
-    <div className="page__container">
-      <div className="page" style={{ backgroundImage: bg }}>
-        <div className="page__overlay">
-          <h1 className="page__title" style={{ color: theme.light }}>
-            {container.name}
-          </h1>
-          {children}
-        </div>
-      </div>
-    </div>
-  );
-};
+const Background = styled('div')(
+  {
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    height: '100%',
+    left: '0',
+    position: 'absolute',
+    top: '0',
+    width: '100%'
+  },
+  props => ({
+    backgroundImage:
+      props.background && props.background.image
+        ? `url('${props.background.image.urls.full}')`
+        : `linear-gradient(to bottom, white, black)`
+  })
+);
+
+const Overlay = styled('div')({
+  background: 'rgba(0, 0, 0, 0.2)',
+  height: '100%',
+  left: '0',
+  position: 'absolute',
+  top: '0',
+  width: '100%'
+});
+
+const PageTitle = styled('h1')({
+  margin: '40px',
+  textAlign: 'center'
+});
+
+export const Page = ({ background, theme, container, children }) => (
+  <PageContainer>
+    <Background background={background}>
+      <Overlay>
+        <PageTitle style={{ color: theme.light }}>{container.name}</PageTitle>
+        {children}
+      </Overlay>
+    </Background>
+  </PageContainer>
+);
 
 Page.propTypes = {
   theme: PropTypes.shape({
