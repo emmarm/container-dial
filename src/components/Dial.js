10564e53a7a0249d6e62054167f0e3438490fa8d
Emma M <mdopure@gmail.com>
2018-09-17 14:19:42 +0700

    check for error loading favicon, use alt icon

--- src/components/Dial.js
+++ src/components/Dial.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 
@@ -39,27 +39,54 @@ const Favicon = styled('div')(
     height: 80,
     width: 80
   },
-  ({ favicon }) => ({
-    background: `center / contain no-repeat url('${favicon}')`
+  ({ favicon, theme, useFavicon }) => ({
+    background: useFavicon && `center / contain no-repeat url('${favicon}')`,
+    color: theme.dark,
+    fontSize: 60,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   })
 );
 
-const getTrimmedName = name => {
-  const lastWordEnd = name.lastIndexOf(' ', 33);
-  return lastWordEnd > -1 && lastWordEnd < 33
-    ? name.slice(0, lastWordEnd) + '...'
-    : name.slice(0, 11) + '...';
-};
+class Dial extends Component {
+  state = { useFavicon: true };
 
-const Dial = ({ dial: { siteUrl, siteName, favicon } }) => {
-  const name = siteName.length > 36 ? getTrimmedName(siteName) : siteName;
-  return (
-    <DialContent href={siteUrl}>
-      <TitleArea>{name}</TitleArea>
-      <Favicon favicon={favicon} />
-    </DialContent>
-  );
-};
+  async componentDidMount() {
+    const { favicon } = this.props.dial;
+
+    const image = new Image();
+    image.src = favicon;
+    await image.complete;
+    image.naturalHeight === 0 && this.setState(() => ({ useFavicon: false }));
+  }
+
+  getTrimmedName = siteName => {
+    const lastWordEnd = siteName.lastIndexOf(' ', 33);
+    return lastWordEnd > -1 && lastWordEnd < 33
+      ? siteName.slice(0, lastWordEnd) + '...'
+      : siteName.slice(0, 11) + '...';
+  };
+
+  getLetter = siteName => siteName.charAt(0).toUpperCase();
+
+  render() {
+    const {
+      dial: { favicon, siteName, siteUrl }
+    } = this.props;
+    const { useFavicon } = this.state;
+    return (
+      <DialContent href={siteUrl}>
+        <TitleArea>
+          {siteName.length > 36 ? this.getTrimmedName(siteName) : siteName}
+        </TitleArea>
+        <Favicon favicon={favicon} useFavicon={useFavicon}>
+          {!useFavicon && this.getLetter(siteName)}
+        </Favicon>
+      </DialContent>
+    );
+  }
+}
 
 Dial.propTypes = {
   dial: PropTypes.shape({
